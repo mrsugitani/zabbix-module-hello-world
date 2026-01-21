347a43b6adad536db155accc0f02753b211d44d8
mrsugitani <98994866+mrsugitani@users.noreply.github.com>
2026-01-22 01:23:00 +0900

    Update hello.world.view.php

--- views/hello.world.view.php
+++ views/hello.world.view.php
@@ -1,21 +1,7 @@
 <?php
 /** @var CView $this */
-
-// Zabbix 6.0/7.0系の標準的なウィジェット構築方法
-$widget = new CWidget();
-$widget->setTitle(_('Hello World Module'));
-
-// コンテンツを囲う箱（Div）を作成
-$div = new CDiv();
-
-// Zabbix標準の余白クラスを適用
-$div->addClass(ZBX_STYLE_LAYOUT_WRAPPER);
-$div->addStyle('padding: 30px;'); // 確実な余白設定
-
-// メッセージを表示
-$div->addItem(new CTag('h1', true, $data['message']));
-$div->addItem(new CTag('p', true, 'This page is a custom module.'));
-
-// ウィジェットにアイテムを追加して表示
-$widget->addItem($div);
-$widget->show();
+?>
+<div style="padding: 50px; background: #fff; margin: 20px; border: 1px solid #dfe4e8;">
+    <h1><?php echo $data['message']; ?></h1>
+    <p>Success! The module is working with padding.</p>
+</div>
