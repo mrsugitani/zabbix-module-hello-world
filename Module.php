fc951e14965fb0d17af4d9469bfe85ec203b8f65
mrsugitani <98994866+mrsugitani@users.noreply.github.com>
2026-01-22 01:03:05 +0900

    Update Module.php

--- Module.php
+++ Module.php
@@ -1,8 +1,15 @@
 <?php
-
-namespace modules\HelloWorld;
+namespace Modules\HelloWorld;
 
 use Zabbix\Core\CModule;
+use APP;
+use CMenuItem;
 
 class Module extends CModule {
+    public function init(): void {
+        APP::Component()->get('menu.main')
+            ->add((new CMenuItem(_('Hello World')))
+                ->setAction('hello.world')
+            );
+    }
 }
