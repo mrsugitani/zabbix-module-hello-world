c3d3f2a83b946ddcbb9870868f4012f4df8cb6fb
mrsugitani <98994866+mrsugitani@users.noreply.github.com>
2026-01-21 21:50:49 +0900

    Update Module.php

--- Module.php
+++ Module.php
@@ -1,18 +1,8 @@
 <?php
+
 namespace HelloWorld;
 
 use Zabbix\Core\CModule;
-use APP;
-use CMenuItem;
 
 class Module extends CModule {
-    public function init(): void {
-        // 「監視データ(Monitoring)」メニューの中に項目を追加
-        APP::Component()->get('menu.main')
-            ->find('Monitoring')
-            ->getSubMenu()
-            ->add((new CMenuItem('Hello World'))
-                ->setAction('hello.world.view')
-            );
-    }
 }
