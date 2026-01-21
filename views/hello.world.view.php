01f1649adb4630e840c5ed7288f3951711663618
mrsugitani <98994866+mrsugitani@users.noreply.github.com>
2026-01-22 01:13:40 +0900

    Update hello.world.view.php

--- views/hello.world.view.php
+++ views/hello.world.view.php
@@ -1,4 +1,21 @@
 <?php
 /** @var CView $this */
-echo '<h1>' . $data['message'] . '</h1>';
-echo '<p>This is a custom Zabbix module page.</p>';
+
+$widget = (new CWidget())
+    ->setTitle(_('Hello World Module')) 
+    ->setDocUrl(CDocHelper::getUrl(CDocHelper::MONITORING_DASHBOARD_VIEW)); 
+
+
+$content = (new CDiv())
+    ->addClass(ZBX_STYLE_LAYOUT_WRAPPER)
+    ->addItem(
+        (new CDiv([
+            (new CTag('h1', true, $data['message'])),
+            (new CTag('p', true, 'This page is a custom module for US client monitoring.'))
+        ]))
+        ->addClass(ZBX_STYLE_OBJ_METABOX)
+        ->addStyle('padding: 20px;')
+    );
+
+
+$widget->addItem($content)->show();
